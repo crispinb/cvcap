aefd8f13627847bc635c1c8d65bba1800401c43f
Crispin Bennett <myself@crisbennett.com>
2022-05-19 20:36:32 +1000

    Dynamic json call example

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,68 @@
 #![allow(dead_code)]
-use std::io::Read;
+#![allow(unused_variables)]
+use reqwest::Client;
 use serde::Deserialize;
-use url::{ParseError, Url};
-// curl --header "X-Client-Token: [token]" "https://checkvist.com/checklists.json"
 
-#[derive(Deserialize, Debug)]
-struct Checklist {
-    id: u32,
+// curl --header "X-Client-Token: [token]" "https://checkvist.com/checklists.json"
+//     .get("https://checkvist.com/checklists.json")
+// current token: aPzOkkaU8ObYKFoMLYHrOlEgOjTytW
+// json return:
+//Object({"archived": Bool(false), "created_at": String("2020/09/13 21:45:52 +0000"), "id": Number(774394), "item_count": Number(16), "markdown?": Bool(true), "name": String("devtest"), "options": Number(3), "percent_completed": Number(0.0), "public": Bool(false), "read_only": Bool(false), "related_task_ids": Null, "tags": Object({"to_review": Bool(false)}), "tags_as_text": String("to_review"), "task_completed": Number(0), "task_count": Number(16), "updated_at": String("2022/04/26 18:41:15 +1000"), "user_count": Number(1), "user_updated_at": String("2022/04/26 18:41:15 +1000")})
+
+#[derive(Debug, Deserialize)]
+pub struct Checklist {
+    id: i32,
     name: String,
-    // tasks
 }
 
+#[derive(Debug)]
+pub struct ChecklistClient {
+    client: Client,
+    base_url: String,
+    api_token: String,
+}
+
+impl ChecklistClient {
+    pub fn new(base_url: String, api_token: String) -> Self {
+        Self {
+            client: Client::new(),
+            base_url,
+            api_token,
+        }
+    }
+
+    pub async fn get_list(&self, list_id: i32, token: &str) -> Result<(), String> {
+        todo!()
+    }
+}
+
+// TODO: get a basic async request working
 // TODO: Replace raw json with a Checklist struct
 // TODO: add a list-mutating call
 // TODO: unit tests
 // TODO: debugger
-fn main() {
-    // let client = reqwest::blocking::Client::new();
-
-    // let mut res = client
-    //     .get("https://checkvist.com/checklists.json")
-    //     .header("X-Client-Token", "urcajLCgk2p9aM5xIu6kGRjBDl2Byo")
-    //     .send()
-    //     .unwrap();
-    // let mut body = String::new();
-    // res.read_to_string(&mut body);
-    // println!("response: {:?}", res);
-    // println!("body: \n{:?}", body);
-
-    // let parsed: Value = serde_json::from_str(&body).unwrap();
-    // println!("json: \n{:?}", parsed);
-
-    println!("{}", get_list(774394, "aPzOkkaU8ObYKFoMLYHrOlEgOjTytW").unwrap());
-}
-
-fn get_list(list_id: u32, token: &str) -> Result<String, ParseError> {
-    let base = Url::parse("https://checkvist.com/checklists/")?;
-    let url = base.join(&(format!("{}{}", list_id, ".json")))?;
-    let client = reqwest::blocking::Client::new();
-    let mut res = client
-        .get(url)
-        .header("X-Client-Token", token)
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    // let c = ChecklistClient::new(
+    //     "https://checkvist.com/checklists.json".into(),
+    //     "aPzOkkaU8ObYKFoMLYHrOlEgOjTytW".into(),
+    // );
+    // c.get_list(9191, "fark!");
+    // println!("I have a {:?}!", c);
+    
+    println!("about to call ...");
+    
+    let list_json: serde_json::Value = 
+        reqwest::Client::new()
+        .get("https://checkvist.com/checklists/774394.json")
+        .header("X-Client-Token", "aPzOkkaU8ObYKFoMLYHrOlEgOjTytW")
         .send()
-        // can't use ? here because reqwest::Error isn't compatible with
-        // ParseError.
-        // What's the typical way to deal with this? Handle errors and
-        // consolidate internally to the function? Or return some wider error type?
-        .unwrap();
+        .await?
+        .json()
+        .await?;
 
-    let mut body = String::new();
-   // TOdO: deal with possible Error value of res 
-    res.read_to_string(&mut body);
-    Ok(body)
+        println!("got: {:?}", list_json);
+        
+        Ok(())
 
-    // let json: Value = serde_json::from_str(&body).unwrap();
-    // Ok(json)
 }
-
-// async approach
-// #[tokio::main]
-// async fn main() -> Result<(), Error> {
-//   let body = reqwest::get("https://checkvist.com/checklists.json")
-//   .await?
-//   .text()
-//   .await?;
-// println!("Body returned! \n {:?}", &body);
-//
-
-//   Ok(())
-// }
