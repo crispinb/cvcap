7e0835a64b86fb5eb06ffd195f82629acfb99f71
Crispin Bennett <myself@crisbennett.com>
2022-05-24 10:32:43 +1000

    Add method to get tasks for list

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,17 @@ use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use url::Url;
 
-// TODO: task method
-// TODO: embed tasks in checklist automatically?
-// TODO: debugger
+// TOdO: investigate error handling for when we get an error with a 200 status code. eg. when
+// unauthenticated. We want in this case to NOT decode json to  our struct. Or to decode to a
+// different struct handling the known errors?
+//
+// TODO: debugger (explore, see if will be useful) [ a debugger is a bit like a REPL for a compiled language]
+// TODO: https://crates.io/crates/secrecy for the token (wrapper type to avoid exposing during logging etc)
 
 // curl --header "X-Client-Token: [token]" "https://checkvist.com/checklists.json"
 //     .get("https://checkvist.com/checklists.json")
-// current token: aPzOkkaU8ObYKFoMLYHrOlEgOjTytW
+// current token: HRpvPJqF4uvwVR8jQ3mkiqlwCm7Y6n
+// if token is bad or expired, receive: `{"message":"Unauthenticated: no valid authentication data in request"}`
 // json return:
 //Object({"archived": Bool(false), "created_at": String("2020/09/13 21:45:52 +0000"), "id": Number(774394), "item_count": Number(16), "markdown?": Bool(true), "name": String("devtest"), "options": Number(3), "percent_completed": Number(0.0), "public": Bool(false), "read_only": Bool(false), "related_task_ids": Null, "tags": Object({"to_review": Bool(false)}), "tags_as_text": String("to_review"), "task_completed": Number(0), "task_count": Number(16), "updated_at": String("2022/04/26 18:41:15 +1000"), "user_count": Number(1), "user_updated_at": String("2022/04/26 18:41:15 +1000")})
 
@@ -21,6 +25,13 @@ pub struct Checklist {
     // TODO: can we automatically convert to a date type of some sort?
     updated_at: String,
     task_count: u16,
+    // TODO: model the rest (task contents)
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Task {
+    id: i32,
+    content: String,
+    // TODO: model the rest
 }
 
 #[derive(Debug)]
@@ -40,13 +51,18 @@ impl ChecklistClient {
         }
     }
 
+    // TAG error_handling:  how best to handle?
+    // boxing errors anonymously here.
+    // a library, custom error type, what?
+    // String here is a stopgap (forcing use of .unwrap)
     pub async fn get_list(&self, list_id: i32) -> Result<Checklist, reqwest::Error> {
-        // TAG error_handling: how to handle ParseError here?
+        // pub async fn get_list(&self, list_id: i32) -> Result<Checklist, Box<dyn std::error::Error>> {
         let url = self
             .base_url
             .join(&format!("/checklists/{}.json", list_id))
             .unwrap();
-        let list: Checklist = self
+
+        let list = self
             .client
             .get(url)
             .header("X-Client-Token", &self.api_token)
@@ -57,24 +73,43 @@ impl ChecklistClient {
 
         Ok(list)
     }
+
+    // and/or a method to get the checklist and all embedded tasks
+    pub async fn get_all_tasks(&self, list_id: i32) -> Result<Vec<Task>, reqwest::Error> {
+        let url = self
+            .base_url
+            .join(&format!("/checklists/{}/tasks.json", list_id))
+            .unwrap();
+        let tasks = self
+            .client
+            .get(url)
+            .header("X-Client-token", &self.api_token)
+            .send()
+            .await?
+            .json()
+            .await?;
+
+        Ok(tasks)
+    }
 }
 
 #[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
+async fn main() {
     let client = ChecklistClient::new(
         "https://checkvist.com/".into(),
-        "aPzOkkaU8ObYKFoMLYHrOlEgOjTytW".into(),
+        "HRpvPJqF4uvwVR8jQ3mkiqlwCm7Y6n".into(),
     );
-    let list = client.get_list(774394).await?;
-
+    let list = client.get_list(774394).await.unwrap();
     println!("list details: {:?}", list);
-    Ok(())
+
+    // let tasks = client.get_all_tasks(774394).await.unwrap();
+    // println!("tasks: {:?}", tasks);
 }
 
 // TODO: move tests when split to lib
 #[cfg(test)]
 mod tests {
-    use super::{Checklist, ChecklistClient};
+    use super::*;
     use wiremock::matchers::{header, method, path};
     use wiremock::{Mock, MockServer, ResponseTemplate};
 
@@ -110,4 +145,26 @@ mod tests {
         // not needed - called automatically
         // mock_server.verify().await;
     }
+
+    #[tokio::test]
+    async fn test_test_list_tasks() {
+        let mock_server = MockServer::start().await;
+        let task = Task {
+            id: 1,
+            content: "content".to_string(),
+        };
+        Mock::given(method("GET"))
+            .and(header("X-Client-Token", "token"))
+            .and(path("/checklists/1/tasks.json"))
+            .respond_with(ResponseTemplate::new(200).set_body_json(vec![task]))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        let client = ChecklistClient::new(mock_server.uri(), "token".to_string());
+        let tasks = client.get_all_tasks(1).await.unwrap();
+        assert_eq!(tasks.len(), 1);
+    }
+
+    // TODO: test for unauthenticated
 }
