45f55fb1bcaecf9b205a751ac8c166e4df33e097
Crispin Bennett <myself@crisbennett.com>
2023-02-20 15:51:53 +1100

    Fix: progress indicator thread not exiting
    - if there was a panic on main thread
    - happened because scoped thread won't die until scope ends, so
      main thread never exits

--- cli/src/progress_indicator.rs
+++ cli/src/progress_indicator.rs
@@ -1,10 +1,12 @@
 use std::io::{stdout, Write};
-use std::sync::mpsc;
-use std::thread;
+use std::sync::mpsc::{self, Sender};
+use std::thread::{self, JoinHandle};
 
 use anyhow::{anyhow, Result};
 
 pub struct ProgressIndicator<'a> {
+    tx: Option<Sender<()>>,
+    handle: Option<JoinHandle<()>>,
     do_before: Box<dyn FnMut() + 'a>,
     progress_char: char,
     display_interval_ms: u64,
@@ -17,6 +19,8 @@ impl ProgressIndicator<'_> {
         interval: u64,
     ) -> ProgressIndicator<'a> {
         ProgressIndicator {
+            tx: None,
+            handle: None,
             do_before,
             progress_char,
             display_interval_ms: interval,
@@ -31,27 +35,40 @@ impl ProgressIndicator<'_> {
 
         let (tx, rx) = mpsc::channel::<()>();
 
-        thread::scope(|s| {
-            s.spawn(move || loop {
+        self.tx = Some(tx);
+        let progress_char = self.progress_char;
+        let display_interval = self.display_interval_ms;
+        // Not using a scoped thread, because it doesn't stop if the supplied job (on main thread)
+        // panics
+        // see https://users.rust-lang.org/t/why-does-a-main-thread-panic-not-exit-the-process-when-a-scoped-thread-is-running/89510/5
+        let handle = thread::Builder::new()
+            .name(String::from("SimpleProgressIndicator-Thread"))
+            .spawn(move || loop {
                 if rx.try_recv().is_ok() {
                     stdout().flush().expect("Couldn't flush stdout");
                     break;
                 };
-                print!("{}", self.progress_char);
+                print!("{}", progress_char);
                 stdout().flush().expect("Couldn't flush stdout");
-                thread::sleep(std::time::Duration::from_millis(self.display_interval_ms));
-            });
-
-            let result = job();
-
-            tx.send(()).map_err(|_e| {
-                anyhow!(String::from(
-                    "Something went wrong stopping progress indicator thread"
-                ))
+                thread::sleep(std::time::Duration::from_millis(display_interval));
             })?;
+        self.handle = Some(handle);
 
-            result
-        })
+        let result = job();
+
+        let error_message = String::from("Couldn't stop progress indicator thread");
+        self.tx
+            .as_ref()
+            .ok_or_else(|| anyhow!("Self.tx is None. Was .start() never called?"))?
+            .send(())
+            .map_err(|_e| anyhow!(error_message.clone()))?;
+
+        self.handle
+            .ok_or_else(|| anyhow!("Self.handle is None"))?
+            .join()
+            .map_err(|_e| anyhow!(error_message))?;
+
+        result
     }
 }
 
